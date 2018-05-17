4c30d8d3cb21f641c0560b91348476753bdc3167
cyij <cyij2006@gmail.com>
2018-05-17 16:21:17 +0800

    iniy

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+Page({
+  data: {
+    toView: 'red',
+    scrollTop: 0,
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    games: [
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+      {
+        gid: '1',
+        name: '斗兽棋',
+        desc: '智商高的人都在玩...',
+        logo: 'https://snsgame.uimg.cn/games/dsq/common/%E6%96%97%E5%85%BD%E6%A3%8B.png',
+        appId: '123'
+      },
+
+    ],
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+})

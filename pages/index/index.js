5659f9e69e11930ae90d95695077275d1fd650a4
cyij <cyij2006@gmail.com>
2018-05-17 16:48:41 +0800

    up

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,11 @@
+var app = getApp();
+var sysInfo = app.globalData.sysInfo;
+var swiperHeight = 150;
 Page({
   data: {
     toView: 'red',
+    swiperHeight: swiperHeight,
+    gamesHeight: (sysInfo.windowHeight - swiperHeight)+'px',
     scrollTop: 0,
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',

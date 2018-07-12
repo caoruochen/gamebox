ebdb1f04c0b43e0c1ab96537997f4e2a9b54b5d4
ysx <ysxfighting@gmail.com>
2018-07-12 15:59:30 +0800

    up

--- pages/more-game/more-game.js
+++ pages/more-game/more-game.js
@@ -1,68 +1,18 @@
 // pages/more-game/more-game.js
-Page({
+var QKPage = require("../../libs/page");
+var http = require("../../util/http");
+var util = require("../../util/util");
 
-  /**
-   * 页面的初始数据
-   */
+QKPage({
+
   data: {
     currentTab: 0,
     pageData: ["pageA", "pageB", "pageC", "pageD", "pageE"],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    var categoryId = options.categoryId;
+    this.loadCategoryData(categoryId)
   },
   
   switchNav: function(e) {
@@ -81,5 +31,13 @@ Page({
     this.setData({
       currentTab: e.detail.current
     })
+  },
+
+/**
+ * 加载分类数据
+ */
+  loadCategoryData: function(categoryId) {
+    console.log("category id="+categoryId)
+    
   }
 })

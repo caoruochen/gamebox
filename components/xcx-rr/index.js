aec0a6765a54adffbc34d34f050152be388b715a
cyij <cyij2006@gmail.com>
2018-07-18 11:36:34 +0800

    添加评论组件

---
+++ components/xcx-rr/index.js
@@ -0,0 +1,13 @@
+var util = require("../../util/util");
+var http = require("../../util/http");
+Component({
+  externalClasses: ['rr-class'],
+  properties: {
+    game: {
+      type: Object,
+      value: null
+    }
+  },
+  methods: {
+  }
+})

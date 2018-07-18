fae0fe9826eef7c65e8c195a2cd805c90f2652b7
caoruochen <2664942980@qq.com>
2018-07-18 19:13:43 +0800

    榜单

--- pages/rank/index.js
+++ pages/rank/index.js
@@ -4,8 +4,54 @@ var util = require("../../util/util");
 var app = getApp();
 
 QKPage({
-  onLoad: function (options) {
-    console.log(options)
-    console.log(app.globalData)
-  }
+	data: {
+		tabs: [
+
+		],
+		imgUrls: [
+			'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+			'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+			'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+		],
+		rankList: [{
+			ranking: '1',
+			name: "韩梅梅",
+			avatar: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83epoLgQ007f6jkTJ5n0RpHAwWR56OOlTuboiaC0ucYEQ3BKMJxwPZ9xlvgibwrCS7YSANms02icYbicyTg/132',
+			gender: '2',
+			score: '3000',
+		}, {
+			ranking: '2',
+			name: "舒克",
+			avatar: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83epoLgQ007f6jkTJ5n0RpHAwWR56OOlTuboiaC0ucYEQ3BKMJxwPZ9xlvgibwrCS7YSANms02icYbicyTg/132',
+			gender: '1',
+			score: '3000',
+		}, {
+			ranking: '3',
+			name: "贝塔",
+			avatar: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83epoLgQ007f6jkTJ5n0RpHAwWR56OOlTuboiaC0ucYEQ3BKMJxwPZ9xlvgibwrCS7YSANms02icYbicyTg/132',
+			gender: '1',
+			score: '3000',
+		}, {
+			ranking: '4',
+			name: "李雷",
+			avatar: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83epoLgQ007f6jkTJ5n0RpHAwWR56OOlTuboiaC0ucYEQ3BKMJxwPZ9xlvgibwrCS7YSANms02icYbicyTg/132',
+			gender: '1',
+			score: '3000',
+		}],
+		activeIndex: 0,
+	},
+	clickTab: function(e) {
+		this.setData({
+			activeIndex: e.currentTarget.dataset.index,
+		});
+	},
+	changeSwiper: function(e) {
+		this.setData({
+			activeIndex: e.detail.current
+		})
+	},
+	onLoad: function(options) {
+		console.log(options)
+		console.log(app.globalData)
+	}
 })

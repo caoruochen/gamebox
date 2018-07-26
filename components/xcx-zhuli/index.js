73d33407817ddb169839da9ed332b1614ef73676
caoruochen <2664942980@qq.com>
2018-07-26 19:13:11 +0800

    fix

--- components/xcx-zhuli/index.js
+++ components/xcx-zhuli/index.js
@@ -15,42 +15,27 @@ Component({
 			type: Boolean,
 			value: true
 		},
-    maxNum: {
-      type: Number,
-      value: 5
-    }
+		maxNum: {
+			type: Number,
+			value: 5
+		}
 	},
 	data: {
 		helpNum: 0,
-	},
-
-	attached: function() {
-		this.changeHelpList();
+		nohelpList: []
 	},
 
 	methods: {
 		changeHelpList: function() {
-			// console.log("changeHelpNum")
 			var len = this.data.helpList.length
-			//还差助力人数
-		  var list = this.data.helpList;
-			if (len < this.data.maxNum) {
-				for (var i = len; i < this.data.maxNum; i++) {
-					list.push(1)
-				}
-				this.setData({
-					helpList: list
-				})
+			var nohelpList = []
+			for (var i = len; i < this.data.maxNum; i++) {
+				nohelpList.push(1)
 			}
-      var helpNum = list.length;
-      for (var i=0; i<list.length; i++) {
-        if (list[i] === 1) {
-          helpNum--;
-        }
-      }
-      this.setData({
-        helpNum: helpNum
-      });
+			this.setData({
+				helpNum: len,
+				nohelpList: nohelpList,
+			})
 		},
 
 		hideHelp: function() {
@@ -60,6 +45,7 @@ Component({
 		},
 
 		handleDel: function(e) {
+			var me = this;
 			wx.showModal({
 				title: '',
 				content: '确定删除此好友的助力分？',
@@ -68,6 +54,24 @@ Component({
 				success: function(res) {
 					if (res.confirm) {
 						//TODO:掉接口删除
+						var duid = e.currentTarget.dataset.uid
+						var index = e.currentTarget.dataset.index
+						http.post('/gamebox/rank/delassist', {
+							aid: app.globalData.zhuliAid,
+							duid: duid,
+						}, function(data) {
+							console.log(data)
+							var helpList = me.data.helpList.concat();
+							helpList.splice(index, 1)
+							me.setData({
+								helpList: helpList,
+							});
+						}, function(code, msg) {
+							wx.showToast({
+								title: msg || '删除失败',
+								icon: 'none'
+							});
+						})
 					}
 				}
 			})

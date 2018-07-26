340c94b43ac8b8b72834a33e53c4ae649dda6837
tuhongwei <1690542111@qq.com>
2018-07-26 10:14:37 +0800

    up

--- pages/my/index.js
+++ pages/my/index.js
@@ -28,7 +28,6 @@ QKPage({
     userInfo: app.globalData.userInfo,
     games: [],
     gameItemWidth: (app.globalData.wwidth-150*ratio) / 5.6,
-    wwidth: app.globalData.wwidth,
     tasks: {
       doing: 0,
       done: 0,

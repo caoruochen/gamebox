92d91e5ef41c8b87df1da8543fdfcde77aafdde8
cyij <cyij2006@gmail.com>
2018-07-30 09:37:24 +0800

    up

--- components/qk-result/index.js
+++ components/qk-result/index.js
@@ -35,16 +35,16 @@ Component({
         show: !!(newVal && app.globalData.startGame)
       });
       if (newVal) {
-        wx.setNavigationBarColor({
-          frontColor: '#ffffff',
-          backgroundColor: '#367be9'
-        });
+        // wx.setNavigationBarColor({
+        //   frontColor: '#ffffff',
+        //   backgroundColor: '#367be9'
+        // });
         app.globalData.startGame = false
       } else {
-        wx.setNavigationBarColor({
-          frontColor: this.data.navBarFontColor,
-          backgroundColor: this.data.navBarBGColor
-        })
+        // wx.setNavigationBarColor({
+        //   frontColor: this.data.navBarFontColor,
+        //   backgroundColor: this.data.navBarBGColor
+        // })
       }
     },
 	}

34d238071633a6d746f18994355d609f98cf6cd2
cyij <cyij2006@gmail.com>
2018-07-31 18:43:47 +0800

    up

--- pages/hit-rank/index.js
+++ pages/hit-rank/index.js
@@ -181,15 +181,13 @@ QKPage({
     }
     
     var data = {};
-    console.log('update')
-    console.log(activity)
     data["activitys[" + ind + "]"] = activity;
     this.setData(data)
   },
   getMoney: function () {
     if (this.data.money < 0.1) {
       wx.showToast({
-        title: '少于0.1元无法提现，请努力挣钱吧',
+        title: '请努力赚钱！少于0.1元无法提现！',
         icon: 'none'
       })
       return;
